b7f089c13708999526f062477fea8dcc9ad329c4
spacelord21 <incrediblespaceee@gmail.com>
2023-04-30 14:24:38 +0200

    google map, use current location hook, circle

---
+++ src/pages/map-location/hooks/use-current-location/use-current-location.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { Region } from "react-native-maps";
+import * as Location from "expo-location";
+import { Alert } from "react-native";
+import * as Permissions from "expo-permissions";
+
+const initialPosition: Region = {
+  latitude: 62,
+  longitude: 91,
+  latitudeDelta: 30,
+  longitudeDelta: 30,
+};
+
+export const useCurrentLocation = () => {
+  const [position, setPosition] = useState<Region>(initialPosition);
+
+  const getCurrentPosition = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert(
+        "Местоположение",
+        "Вы запретили доступ приложению к Вашей геолокации. Вы можете выдать разрешение в настройках",
+        [
+          {
+            style: "cancel",
+            text: "ОК",
+          },
+        ]
+      );
+    }
+    const { coords } = await Location.getCurrentPositionAsync();
+    setPosition({
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+      latitudeDelta: 0.03,
+      longitudeDelta: 0.03,
+    });
+  };
+
+  useEffect(() => {
+    getCurrentPosition();
+  }, []);
+
+  return { position };
+};
--- src/pages/map-location/map-location.tsx
+++ src/pages/map-location/map-location.tsx
@@ -1,9 +1,48 @@
-import YaMap from "react-native-yamap";
+import MapView, { Circle } from "react-native-maps";
+import { useCurrentLocation } from "./hooks";
+import { PrimaryButton, styled } from "@shared/ui";
+import { useTheme } from "styled-components";
+
+const Flex = styled.View`
+  flex: 0.8;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Map = styled(MapView)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const radius = 500;
+const circleWidth = 1.5;
 
 export const MapLocation = () => {
+  const { position } = useCurrentLocation();
+  const theme = useTheme();
+
   return (
-    <>
-      <YaMap style={{ flex: 1 }} />
-    </>
+    <Container>
+      <Map initialRegion={position} showsUserLocation region={position}>
+        <Circle
+          center={{
+            latitude: position.latitude,
+            longitude: position.longitude,
+          }}
+          radius={radius}
+          strokeColor={theme.palette.text.primary}
+          strokeWidth={circleWidth}
+        />
+      </Map>
+      <Flex />
+      <PrimaryButton onPress={() => {}}>Далее</PrimaryButton>
+    </Container>
   );
 };
